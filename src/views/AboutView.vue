97c048418b6b3ba91e2f2f34794c0656129478f8
AClaich <claich.alexis@gmail.com>
2023-12-22 11:44:26 +0100

    install excel js

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
@@ -13,3 +13,30 @@
   }
 }
 </style>
+
+ -->
+
+
+ <style>
+ a-button{
+   cursor: pointer;
+ }
+</style>
+
+<template>
+ <a-form @submit="handleSubmit">
+   <!-- Ajoutez ici les champs du formulaire pour le Faire-Part -->
+   <a-form-item>
+     <a-button type="primary" html-type="submit">Envoyer la réponse</a-button>
+   </a-form-item>
+ </a-form>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { Form, FormItem, Input, Button } from 'ant-design-vue';
+
+const handleSubmit = (item: any) => {
+  console.log('handleSubmit')
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<!-- <script>
+import { defineProps } from 'vue';
+import { Form, FormItem, Input, Button } from 'ant-design-vue';
+</script> -->
 
-<template>
+<!-- <template>
   <main>
     <TheWelcome />
   </main>
+</template> -->
+
+
+<template>
+  <a-form @submit="handleSubmit()">
+    <a-form-item label="Nom d'utilisateur">
+      <a-input :v-bind="username" />
+    </a-form-item>
+    <a-form-item label="Mot de passe">
+      <a-input type="password" :v-model="password" />
+    </a-form-item>
+    <a-form-item>
+      <a-button type="primary" html-type="submit">Se connecter</a-button>
+    </a-form-item>
+  </a-form>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+      username: '',
+      password: '',
+}>()
+
+const handleSubmit = () => {
+  // Ajoutez ici la logique de gestion de la connexion avec Vuex ou une requête HTTP
+  // Enregistrez l'état de connexion dans votre store Vuex si nécessaire
+}
+</script>
