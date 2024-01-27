2081f203ef98359af646907df6b8c2c1a14c19cb
ECOCERT\aclaich <aclaich@ecocert.com>
2024-01-27 13:34:59 +0100

    arrange store

--- src/store/index.ts
+++ src/store/index.ts
@@ -17,9 +17,11 @@ const store = createStore<State>({
   mutations: {
     getListUser(state: State) {
       try {
-        axios.get("http://localhost:4200/api/dataconnexions").then((response) => {
-          state.listUser = response.data;
-        });
+        axios
+          .get("http://localhost:4200/api/dataconnexions")
+          .then((response) => {
+            state.listUser = response.data;
+          });
       } catch (error) {
         console.error(error);
       }
@@ -27,13 +29,13 @@ const store = createStore<State>({
     setCurrentUser(state: State, user: UserEntity) {
       state.currentUser = state.currentUser || { user1: null, user2: null };
       if (user.prenom.includes("&")) {
-        let user1: UserEntity = {...user};
-        let user2: UserEntity = {...user};
+        let user1: UserEntity = { ...user };
+        let user2: UserEntity = { ...user };
         user1.prenom = user.prenom.split("&")[0].trim();
         user1.nom = user.nom.split("&")[0].trim();
         user2.prenom = user.prenom.split("&")[1].trim();
         user2.nom = user.nom.split("&")[1].trim() ?? user.nom;
-        
+
         state!.currentUser!.user1 = user1;
         state!.currentUser!.user2 = user2;
       } else {
@@ -51,25 +53,18 @@ const store = createStore<State>({
       state.currentUser = null;
       localStorage.removeItem("currentUser");
     },
-    sendFormResult(state: State, user: UserEntity) {
-      state.currentUser = state.currentUser || { user1: null, user2: null };
-      if (user.prenom.includes("&")) {
-        let user1: UserEntity = {...user};
-        let user2: UserEntity = {...user};
-        user1.prenom = user.prenom.split("&")[0].trim();
-        user1.nom = user.nom.split("&")[0].trim();
-        user2.prenom = user.prenom.split("&")[1].trim();
-        user2.nom = user.nom.split("&")[1].trim() ?? user.nom;
-        
-        state!.currentUser!.user1 = user1;
-        state!.currentUser!.user2 = user2;
-      } else {
-        state!.currentUser!.user1 = user;
-        state!.currentUser!.user2 = null;
-      }
+    sendFormResult(state: State, user1: UserEntity, user2: UserEntity) {
+      const user: UserEntity = {
+        ...user1,
+        jeudi1: user1.jeudi1 ? 1 : 0,
+        vendredi1: user1.vendredi1 ? 1 : 0,
+        samedi1: user1.samedi1 ? 1 : 0,
+        logement1: user1.logement1 ? 1 : 0,
+        prenom: user1.prenom,
+      };
 
       localStorage.setItem("currentUser", JSON.stringify(state.currentUser));
-    }
+    },
   },
   getters: {
     currentUser(state: State) {
@@ -77,5 +72,53 @@ const store = createStore<State>({
     },
   },
 });
+
+const mapStateUserToUserEntity = (stateUser: { user1: UserEntity; user2: UserEntity; }) => {
+  const user: UserEntity = {
+    ...stateUser.user1!,
+    prenom: stateUser.user1.prenom + (stateUser.user2?.prenom ?? ""),
+    nom: stateUser.user1.nom + (stateUser.user2?.nom ?? ""),
+    presenceVin1: stateUser.user1.presenceVin1,
+    presenceReception1: stateUser.user1.presenceReception1,
+    presenceRetour1: stateUser.user1.presenceRetour1,
+    vegetarien1: stateUser.user1.vegetarien1,
+    jeudi1: stateUser.user1.jeudi1,
+    vendredi1: stateUser.user1.vendredi1,
+    samedi1: stateUser.user1.samedi1,
+    logement1: stateUser.user1.logement1,
+    presenceVin2: stateUser.user2.presenceVin2 ?? 0,
+    presenceReception2: stateUser.user2.presenceReception2 ?? 0,
+    presenceRetour2: stateUser.user2.presenceRetour2 ?? 0,
+    vegetarien2: stateUser.user2.vegetarien2 ?? 0,
+    jeudi2: stateUser.user2.jeudi2 ?? 0,
+    vendredi2: stateUser.user2.vendredi2 ?? 0,
+    samedi2: stateUser.user2.samedi2 ?? 0,
+    logement2: stateUser.user2.logement2 ?? 0,
+  };
+
+  return user;
+};
+
+
+const mapUserEntityToStateUser = (user: UserEntity) => {
+  const stateUser: { user1: UserEntity | null; user2: UserEntity | null; } = {
+    user1: {...user},
+    user2: {...user},
+  };
+
+  if (user.prenom.includes("&")) {
+    stateUser.user1!.prenom = user.prenom.split("&")[0].trim();
+    stateUser.user2!.prenom = user.prenom.split("&")[1].trim();
+    if (user.nom.includes("&")) {
+      stateUser.user1!.nom = user.nom.split("&")[0].trim();
+      stateUser.user2!.nom = user.nom.split("&")[1].trim();
+    }
+  } else {
+    stateUser.user1 = user;
+    stateUser.user2 = null;
+  }
+
+  return stateUser;
+};
 
 export default store;
